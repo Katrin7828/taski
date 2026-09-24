b63688a0321e2a1a3a27fc22038c4da33223015f
Ubuntu <ubuntu@fv4d4uqtkfjm2g360vqv.auto.internal>
2026-09-24 13:13:29 +0000

    проект доделан

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.155.91', '127.0.0.1', 'localhost', 'taskiya.hopto.org'] 
 
 
 # Application definition
@@ -89,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -100,13 +100,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
+STATIC_URL = '/static_backend/'
 
-STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / 'static_backend' 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
